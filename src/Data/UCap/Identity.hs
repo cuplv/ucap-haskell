c56a23e462a4d2ef3e293a7b476cba6c285bac63
octalsrc <nicklewchenko92@gmail.com>
2021-12-06 13:49:02 -0700

    Add IdentityC cap/effect domain

---
+++ src/Data/UCap/Identity.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Data.UCap.Identity where
+
+import Data.UCap.Classes
+
+import Data.Aeson
+import GHC.Generics
+
+data IdentityC s = IdentityC deriving (Show,Eq,Ord,Generic)
+
+instance ToJSON (IdentityC s) where
+  toEncoding = genericToEncoding defaultOptions
+instance FromJSON (IdentityC s)
+
+instance Semigroup (IdentityC s) where
+  IdentityC <> IdentityC = IdentityC
+
+instance Monoid (IdentityC s) where
+  mempty = IdentityC
+
+instance Meet (IdentityC s) where
+  meet IdentityC IdentityC = IdentityC
+  IdentityC <=? IdentityC = True
+
+instance BMeet (IdentityC s) where
+  meetId = IdentityC
+
+instance Split (IdentityC s)
+
+instance EffectDom (IdentityC s) where
+  type State (IdentityC s) = s
+  eFun IdentityC = id
+
+instance Cap (IdentityC s) where
+  type Effect (IdentityC s) = IdentityC s
