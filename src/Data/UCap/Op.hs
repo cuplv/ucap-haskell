cc148ca923ee977db4800cee2e3cdba97f7779d1
octalsrc <nicklewchenko92@gmail.com>
2021-12-17 09:45:08 -0700

    Define Op

--- src/Data/UCap/Op.hs
+++ src/Data/UCap/Op.hs
@@ -22,8 +22,32 @@ instance
   , State e ~ s
   ) => Applicative (Op c e s) where
   pure a = Op uniC idC (const $ (idE,a))
-  (Op r1 w1 b1) <*> (Op r2 w2 b2) = 
+  Op r1 w1 b1 <*> Op r2 w2 b2 =
     Op (r1 `meet` r2) (w1 <> w2) $ \s ->
       let (e1,f) = b1 s
           (e2,a) = b2 (eFun e1 s)
       in (e2 <> e1, f a)
+
+mkOp :: c -> c -> (State' c -> (Effect c,a)) -> Op' c a
+mkOp = Op
+
+newtype CapFail c = CapFail (Maybe c, Maybe c)
+
+runOp
+  :: (Cap c)
+  => c
+  -> c
+  -> State' c
+  -> Op' c a
+  -> Either (CapFail c) (c,Effect c,a)
+runOp rr rw s (Op r w f) = case (rr <=? r, split rw w) of
+  (True, Just rw') -> let (e,a) = f s
+                      in Right (rw',e,a)
+  (rf, wf) -> Left $ CapFail
+    (if rf
+        then Nothing
+        else Just r
+    ,case wf of
+        Just _ -> Nothing
+        Nothing -> Just w
+    )
