899b1ec051ddd429eb71a066fef751113df7948e
octalsrc <nicklewchenko92@gmail.com>
2022-01-04 10:28:19 -0700

    Operation combinators

--- src/Data/UCap/Op.hs
+++ src/Data/UCap/Op.hs
@@ -4,50 +4,82 @@ module Data.UCap.Op where
 
 import Data.UCap.Classes
 
+data OpBody e s a
+  = OpBody { opBodyFun :: s -> Maybe (e,a) }
+
+instance Functor (OpBody e s) where
+  fmap f (OpBody b) = OpBody $
+    (\s -> (\(e,a) -> (e, f a)) <$> b s)
+
+opBFail :: OpBody e s a
+opBFail = OpBody $ const Nothing
+
+opBEffect :: e -> OpBody e s ()
+opBEffect e = OpBody . const $ Just (e, ())
+
+opBRead :: (Monoid e) => OpBody e s s
+opBRead = OpBody $ \s -> Just (idE,s)
+
+opBTest :: (Monoid e) => (s -> Bool) -> OpBody e s ()
+opBTest t = OpBody $ \s ->
+  if t s
+     then Just (idE, ())
+     else Nothing
+
+instance (EffectDom e, EDState e ~ s) => Applicative (OpBody e s) where
+  pure a = OpBody . const $ Just (idE,a)
+  OpBody b1 <*> OpBody b2 = OpBody $ \s -> case b1 s of
+    Just (e1,f) -> case b2 (eFun e1 s) of
+      Just (e2,a) -> Just (e1 <> e2, f a)
+      Nothing -> Nothing
+    Nothing -> Nothing
+
+instance (EffectDom e, EDState e ~ s) => Monad (OpBody e s) where
+  return = pure
+  OpBody b1 >>= m = OpBody $ \s -> case b1 s of
+    Just (e1,a1) -> case opBodyFun (m a1) (eFun e1 s) of
+      Just (e2,a2) -> Just (e1 <> e2, a2)
+      Nothing -> Nothing
+    Nothing -> Nothing
+
 data Op c e s a
   = Op { opRead :: c
        , opWrite :: c
-       , opBody :: s -> (e,a)
+       , opProduces :: c
+       , opBody :: OpBody e s a
        }
 
 type Op' c = Op c (CEffect c) (CState c)
 
 instance Functor (Op c e s) where
-  fmap f (Op r w b) = Op r w $ (fmap.fmap) f b
+  fmap f (Op r w p b) = Op r w p (fmap f b)
 
 instance
-  ( Monoid c
-  , BMeet c
-  , EffectDom e
-  , EDState e ~ s
+  ( Cap c
+  , CEffect c ~ e
+  , CState c ~ s
   ) => Applicative (Op c e s) where
-  pure a = Op uniC idC (const $ (idE,a))
-  Op r1 w1 b1 <*> Op r2 w2 b2 =
-    Op (r1 `meet` r2) (w1 <> w2) $ \s ->
-      let (e1,f) = b1 s
-          (e2,a) = b2 (eFun e1 s)
-      in (e2 <> e1, f a)
+  pure a = Op uniC idC idC (pure a)
+  Op r1 w1 p1 b1 <*> Op r2 w2 p2 b2 = case (split w2 p1, split p1 w2) of
+    (Just w2',_) -> Op (r1 `meet` r2) (w1 <> w2') p2 (b1 <*> b2)
+    (_,Just p1') -> Op (r1 `meet` r2) w1 (p1' <> p2) (b1 <*> b2)
+    _ -> Op (r1 `meet` r2) (w1 <> w2) (p1 <> p2) (b1 <*> b2)
 
-mkOp :: c -> c -> (CState c -> (CEffect c,a)) -> Op' c a
-mkOp = Op
+opFail :: (Cap c) => Op c e s a
+opFail = Op uniC idC uniC opBFail
 
-newtype CapFail c = CapFail (Maybe c, Maybe c)
+opEffect :: (Cap c, CEffect c ~ e) => e -> Op c e s ()
+opEffect e = Op uniC (mincap e) (undo e) (opBEffect e)
 
-runOp
-  :: (Cap c)
+opQuery :: (Cap c, Monoid e) => c -> Op c e s s
+opQuery c = Op c idC idC opBRead
+
+opTest
+  :: (Cap c, CEffect c ~ e, CState c ~ s)
   => c
-  -> c
-  -> CState c
-  -> Op' c a
-  -> Either (CapFail c) (c,CEffect c,a)
-runOp rr rw s (Op r w f) = case (rr <=? r, split rw w) of
-  (True, Just rw') -> let (e,a) = f s
-                      in Right (rw',e,a)
-  (rf, wf) -> Left $ CapFail
-    (if rf
-        then Nothing
-        else Just r
-    ,case wf of
-        Just _ -> Nothing
-        Nothing -> Just w
-    )
+  -> (s -> Bool)
+  -> Op c e s ()
+opTest c t = Op c idE idC (opBTest t)
+
+mkOp :: (Monoid c) => c -> c -> (s -> (e,a)) -> Op c e s a
+mkOp r w f = Op r w idC (OpBody (Just . f))
