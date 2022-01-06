d94edea82d4a3c2a1257182d839d21ead24a6082
octalsrc <nicklewchenko92@gmail.com>
2022-01-06 16:47:20 -0700

    Add operations for Counter

--- src/Data/UCap/Op.hs
+++ src/Data/UCap/Op.hs
@@ -2,12 +2,14 @@
 
 module Data.UCap.Op
   ( module Data.UCap.Editor
+  , module Data.UCap.Op.Counter
   , module Data.UCap.Op.Either
   , module Data.UCap.Op.Internal
   , module Data.UCap.Op.Map
   ) where
 
 import Data.UCap.Editor
+import Data.UCap.Op.Counter
 import Data.UCap.Op.Either
 import Data.UCap.Op.Internal
 import Data.UCap.Op.Map
--- src/Data/UCap/Op/Counter.hs
+++ src/Data/UCap/Op/Counter.hs
@@ -13,8 +13,17 @@ atMost n = opTest
   subAny
   (<= n)
 
+subOp :: (Num n, Ord n) => n -> Op' (CounterC n) n
+subOp amt = opEffect (subE amt) *> pure (pure amt)
+
 preSub :: (Num n, Ord n) => PreOp (CounterC n) n n
 preSub = mkPre uniC subAny $ \n -> opBEffect (subE n) >> return n
 
-subGuard :: (Num n, Ord n) => n -> n -> Op' (CounterC n) ()
-subGuard lim amt = atLeast lim *> opEffect (subE amt)
+subGuard :: (Num n, Ord n) => n -> n -> Op' (CounterC n) n
+subGuard lim amt = atLeast lim *> subOp amt
+
+addOp :: (Num n, Ord n) => n -> Op' (CounterC n) n
+addOp amt = opEffect (addE amt) *> pure (pure amt)
+
+preAdd :: (Num n, Ord n) => PreOp (CounterC n) n n
+preAdd = mkPre uniC addAny $ \n -> opBEffect (addE n) >> return n
