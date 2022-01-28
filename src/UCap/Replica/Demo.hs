9036574890c0c4923c05e295b05017c52b98d98e
octalsrc <nicklewchenko92@gmail.com>
2022-01-27 21:18:33 -0700

    Implement demo network actions

--- src/UCap/Replica/Demo.hs
+++ src/UCap/Replica/Demo.hs
@@ -1,7 +1,17 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 
-module UCap.Replica.Demo where
+module UCap.Replica.Demo
+  ( DemoState
+  , transactD
+  , observeD
+  , stateD
+  , transferD
+  , acceptD
+  , maskD
+  , capsL
+  , liftDemo
+  ) where
 
 import UCap
 import UCap.Lens
@@ -20,15 +30,34 @@ import qualified Data.Map as Map
 
 type RState i c = (VThread i (CEffect c), Map i (Capconf i c))
 
+{-| A monad for simulating transactions on a network of store replicas. -}
 type DemoState i c m =
        ExceptT ()
          (ReaderT (CState c)
             (StateT (RState i c) m))
 
-transactD :: (Ord i, Cap c, Monad m) => i -> Op c m () b -> DemoState i c m b
-transactD = undefined
+{-| Run an 'Op' on the given process.  If the process does not have
+  sufficient capabilities, a 'Nothing' value is returned. -}
+transactD
+  :: (Ord i, Cap c, Monad m)
+  => i
+  -> Op c m () b
+  -> DemoState i c m (Maybe b)
+transactD i op = do
+  cc <- use (capsL i)
+  let caps = Caps (remoteG' i cc) (localG i cc)
+  s <- stateD i
+  case execWith caps s op of
+    Just act -> do
+      (_,e,b) <- liftDemo act
+      case consumeG i e cc of
+        Just cc' -> do
+          _1 %= event i e
+          capsL i .= cc'
+          return (Just b)
+    Nothing -> return Nothing
 
-{-| @observeD i1 i2@ makes events from (and seen by) @i2@ visible to
+{-| @'observeD' i1 i2@ makes events from (and seen by) @i2@ visible to
   @i1@. -}
 observeD :: (Ord i, Cap c, Monad m) => i -> i -> DemoState i c m ()
 observeD i1 i2 = do
@@ -36,6 +65,40 @@ observeD i1 i2 = do
   use (_2 . at i2) >>= \case
     Just cc2 -> _2 %= Map.adjust (<> cc2) i1
     Nothing -> return ()
+
+{-| @'maskD' i1 (i2,c)@ applies @c@ as a mask to @i1@'s capability, on
+  request from @i2@. -}
+maskD :: (Ord i, Cap c, Monad m) => i -> (i,c) -> DemoState i c m ()
+maskD i1 (i2,c) = capsL i1 %= maskG i1 (i2,c)
+
+{-| @'transferD' i1 (i2,c)@ transfers the capability @c@ from @i1@ to
+  @i2@.  If @i1@'s capabilities are not sufficient to do this, an
+  exception is thrown in the underying 'ExceptT' monad.
+
+@
+foo :: ('Ord' i, 'Cap' c, 'Monad' m) => 'DemoState' i c m 'String'
+foo = do
+  result <- 'runExceptT' $ 'transferD' i1 (i2,c)
+  case result of
+    'Right' () -> do
+      'acceptD' i2
+      'return' "The transfer succeeded."
+    'Left' () -> 'return' "The transfer failed."
+@
+-}
+transferD :: (Ord i, Cap c, Monad m)
+  => i
+  -> (i,c)
+  -> DemoState i c (ExceptT () m) ()
+transferD i1 (i2,c) = do
+  cc <- use $ capsL i1
+  case transferG i1 (i2,c) cc of
+    Just cc' -> capsL i1 .= cc'
+    Nothing -> throwError ()
+
+{-| Accept transferred capabilities on the given process. -}
+acceptD :: (Ord i, Cap c, Monad m) => i -> DemoState i c m ()
+acceptD i = capsL i %= acceptG i
 
 {-| Get the initial state. -}
 initialState :: (Monad m) => DemoState i c m (CState c)
@@ -52,3 +115,10 @@ stateD
   => i
   -> DemoState i c m (CState c)
 stateD i = eFun <$> allEffectsD i <*> initialState
+
+{-| A lens to the 'Capconf' for a process ID. -}
+capsL :: (Ord i, Cap c) => i -> Lens' (RState i c) (Capconf i c)
+capsL i = _2 . at i . non mempty
+
+liftDemo :: (Monad m) => m a -> DemoState i c m a
+liftDemo = lift . lift . lift
