ef0d83a083ad07fe3e82b1dab4c738284c74fc36
octalsrc <nicklewchenko92@gmail.com>
2022-02-03 14:34:31 -0700

    Add demo tests

--- test/UCap/Replica/PDemoTests.hs
+++ test/UCap/Replica/PDemoTests.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
+
 module UCap.Replica.PDemoTests (testPDemo) where
 
 import UCap.Domain
+import UCap.Lens
 import UCap.Op
 import UCap.Replica
 import UCap.Replica.Demo
@@ -24,14 +27,30 @@ type IntOp = Op (CounterC Int) Identity ()
 alpha = "alpha"
 beta = "beta"
 
+-- | A looping script that pops transactions on an 'Int' state from
+-- the given queue, and executes them.
+intModder
+  :: (Ord i)
+  => Lens' s [IntOp ()]
+  -> PScript i (CounterC Int) (State s)
+intModder l = do
+  t <- await [popQ l]
+  transact $ liftOpM t
+  intModder l
+
+intModder2
+  :: (Ord i)
+  => Lens' s [IntOp ()]
+  -> Lens' s [IntOp ()]
+  -> PScript i (CounterC Int) (State s)
+intModder2 l1 l2 = do
+  t <- await [popQ l1, popQ l2]
+  transact $ liftOpM t
+  intModder2 l1 l2
+
 tqueues = testGroup "Transaction queues" $
-  let rsc :: PScript String (CounterC Int) (State [IntOp ()])
-      rsc = do
-          t <- await [popQ id]
-          transact $ liftOpM t
-          rsc
-      act :: PDemo String (CounterC Int) (State [IntOp ()]) (Int,Int,Int)
-      act = do
+  let act1 :: PDemo String (CounterC Int) (State [IntOp ()]) (Int,Int,Int)
+      act1 = do
         liftPDemo $ modify (++ [effect (addE 3)])
         loopPD
         s1 <- lift $ stateD alpha
@@ -41,10 +60,31 @@ tqueues = testGroup "Transaction queues" $
         loopPD
         s3 <- lift $ stateD alpha
         return (s1,s2,s3)
-      cc0 = mkUniform uniC [alpha]
-      m = Map.singleton alpha rsc
+      cc0 = mkUniform (uniC :: CounterC Int) [alpha,beta]
+      m1 = Map.singleton alpha (intModder id)
+      act2 = do
+        loopPD
+        liftPDemo $ _1 %= (++ [effect (addE 3)])
+        liftPDemo $ _2 %= (++ [effect (addE 4)])
+        s1 <- lift $ stateD alpha
+        loopPD
+        s2 <- lift $ stateD alpha
+        liftPDemo $ _2 %= (++ [effect (addE 1)])
+        loopPD
+        s3 <- lift $ stateD alpha
+        return (s1,s2,s3)
+      m2 = Map.singleton alpha (intModder2 _1 _2)
+      act3 = do
+        liftPDemo $ _1 %= (++ [effect (addE 3)])
+        liftPDemo $ _2 %= (++ [effect (addE 4)])
+        evalRep alpha
+        lift $ stateD alpha
   in [testCase "single queue" $
-        evalState (evalPDemo m cc0 0 act) [] @?= (3,3,18)
+        evalState (evalPDemo m1 cc0 0 act1) [] @?= (3,3,18)
+     ,testCase "double queue" $
+        evalState (evalPDemo m2 cc0 0 act2) ([],[]) @?= (0,7,8)
+     ,testCase "evalRep" $
+        evalState (evalPDemo m2 cc0 0 act3) ([],[]) @?= 7
      ]
 
 loops = testGroup "Loops" $
