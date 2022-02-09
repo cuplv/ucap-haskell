4321ab2663eea71e70869466317232b777e739b5
octalsrc <nicklewchenko92@gmail.com>
2022-02-09 11:21:21 -0700

    Define SRQueue, SECell, to simplify CRDT defs

---
+++ src/Data/SRQueue.hs
@@ -0,0 +1,100 @@
+module Data.SRQueue
+  ( SRQueue
+  , srEmpty
+  , srInit
+  , srEnqueue
+  , srEnqueueAll
+  , srDequeue
+  , srDequeueAll
+  , srLength
+  , SECell
+  , seInit
+  , seSet
+  , seGet
+  ) where
+
+{-| An 'SRQueue' is a queue of elements that can be safely added-to in a
+  concurrent way.  This means that two added-to queues can be derived
+  from a common ancestor and then merged ('<>'), and no elements will
+  be lost or duplicated.
+
+@
+let q = 'srInit' [1,2]
+    q3 = 'srEnqueueAll' [3,0,5] q
+    q4 = 'srEnqueue' 4 q
+in  'srDequeueAll' (q3 '<>' q4) == [1,2,3,0,4,5]
+    '&&' 'srDequeueAll' (q4 '<>' q3) == [1,2,3,0,4,5]
+@
+
+  The order of concurrent elements is determined by their 'Ord'
+  instance.
+-}
+data SRQueue a
+  = SRQueue { lqConsumed :: Int
+            , lqElements :: [a]
+            }
+  deriving (Show,Eq,Ord)
+
+instance (Ord a) => Semigroup (SRQueue a) where
+  SRQueue g1 as1 <> SRQueue g2 as2 =
+    let fr (a1:as1) (a2:as2) | a1 == a2 = a1 : fr as1 as2
+                             | a1 > a2 = a1 : fr as1 (a2:as2)
+                             | a1 < a2 = a2 : fr (a1:as1) as2
+        g' = max g1 g2
+    in SRQueue g' $ fr (drop g' as1) (drop g' as2)
+
+instance (Ord a) => Monoid (SRQueue a) where
+  mempty = srEmpty
+
+{-| An 'SRQueue' containing no elements. -}
+srEmpty :: SRQueue a
+srEmpty = srInit []
+
+{-| An 'SRQueue' containing only the given elements. -}
+srInit :: [a] -> SRQueue a
+srInit as = SRQueue 0 as
+
+{-| Add an element to the queue.  This is safe to perform concurrently with other 'srEnqueue' changes. -}
+srEnqueue :: a -> SRQueue a -> SRQueue a
+srEnqueue a = srEnqueueAll [a]
+-- srEnqueue a (SRQueue g as) = SRQueue g (as ++ [a])
+
+srEnqueueAll :: [a] -> SRQueue a -> SRQueue a
+srEnqueueAll as1 (SRQueue g as2) = SRQueue g (as1 ++ as2)
+
+{-| Pop the next element from the queue (if it exists).  This must not
+  be performed concurrently with other 'srDequeue' or 'srDequeueAll'
+  changes. -}
+srDequeue :: SRQueue a -> Maybe (SRQueue a, a)
+srDequeue (SRQueue g (a:as)) = Just (SRQueue (g + 1) as, a)
+srDequeue _ = Nothing
+
+{-| Consume the entire queue as a list.  Just like 'srDequeue', this
+  must not be performed concurrently to other 'srDequeue' or
+  'srDequeueAll' changes. -}
+srDequeueAll :: SRQueue a -> (SRQueue a, [a])
+srDequeueAll (SRQueue g as) = (SRQueue (g + length as) [], as)
+
+{-| Get the length of the queue. -}
+srLength :: SRQueue a -> Int
+srLength (SRQueue _ as) = length as
+
+data SECell a
+  = SECell Int a
+  deriving (Show,Eq,Ord)
+
+instance (Eq a) => Semigroup (SECell a) where
+  SECell g1 a1 <> SECell g2 a2
+    | g1 > g2 = SECell g1 a1
+    | g1 < g2 = SECell g2 a2
+    | a1 == a2 = SECell g1 a1
+    | otherwise = error "Conflicting cell values."
+
+seInit :: a -> SECell a
+seInit = SECell 0
+
+seSet :: a -> SECell a -> SECell a
+seSet a (SECell g _) = SECell (g + 1) a
+
+seGet :: SECell a -> a
+seGet (SECell _ a) = a
--- src/UCap/Coord/Classes.hs
+++ src/UCap/Coord/Classes.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module UCap.Coord.Classes where
 
+import Data.SRQueue
 import UCap.Domain.Classes
+import UCap.Lens
+
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 type family GEffect g where
   GEffect g = CEffect (GCap g)
@@ -12,7 +18,7 @@ type family GEffect g where
 type family GState g where
   GState g = CState (GCap g)
 
-class (Cap (GCap g), Monoid g) => CoordSys g where
+class (Cap (GCap g)) => CoordSys g where
 
   {-| The type of capabilities (which also defines the state and effect
   types). -}
@@ -37,3 +43,70 @@ class (Cap (GCap g), Monoid g) => CoordSys g where
 
   {-| The locally-held read and write capabilities. -}
   localCaps :: GId g -> g -> Caps (GCap g)
+
+data Token i
+  = Token { _tkOwner :: SECell i
+          , _tkQueue :: SRQueue i
+          }
+  deriving (Show,Eq,Ord)
+
+makeLenses ''Token
+
+instance (Ord i) => Semigroup (Token i) where
+  Token o1 q1 <> Token o2 q2 = Token (o1 <> o2) (q1 <> q2)
+
+mkToken :: i -> Token i
+mkToken i = Token (seInit i) srEmpty
+
+tokenOwner :: Token i -> i
+tokenOwner (Token o _) = seGet o
+
+isRequestedOf :: (Eq i) => i -> Token i -> Bool
+isRequestedOf i (Token o q) = i == seGet o && srLength q > 0
+
+requestToken :: (Ord i) => i -> Token i -> Token i
+requestToken i tk@(Token o q)
+  | i == seGet o = tk
+  | otherwise = Token o (srEnqueue i q)
+
+data GrantError
+  = NotOwner
+  | NotRequested
+  deriving (Show,Eq,Ord)
+
+grantToken :: (Eq i) => i -> Token i -> Either GrantError (Token i)
+grantToken i (Token o q) = case srDequeue q of
+  Just (q2,i2) | i == seGet o -> Right $ Token (seSet i2 o) q2
+               | otherwise -> Left NotOwner
+  Nothing -> Left NotRequested
+
+data EscrowIntRequest i
+  = EscrowIntRequest { _eprAsker :: i
+                     , _eprAmount :: Int
+                     }
+  deriving (Show,Eq,Ord)
+
+data EscrowIntAccount i
+  = EscrowIntAccount { _epaOwned :: SECell Int
+                     , _epaInbox :: SRQueue Int
+                     , _epaRequests :: SRQueue (EscrowIntRequest i)
+                     }
+  deriving (Show,Eq,Ord)
+
+initAccount :: Int -> EscrowIntAccount i
+initAccount n = EscrowIntAccount (seInit n) srEmpty srEmpty
+
+instance (Ord i) => Semigroup (EscrowIntAccount i) where
+  EscrowIntAccount o1 i1 r1 <> EscrowIntAccount o2 i2 r2 =
+    EscrowIntAccount (o1 <> o2) (i1 <> i2) (r1 <> r2)
+
+data EscrowIntPool i
+  = EscrowPool { _epAccounts :: Map i (EscrowIntAccount i)
+               , _epSinks :: [i]
+               , _epSource :: [i]
+               }
+  deriving (Show,Eq,Ord)
+
+instance (Ord i) => Semigroup (EscrowIntPool i) where
+  EscrowPool a1 sk1 sr1 <> EscrowPool a2 _ _
+    = EscrowPool (Map.unionWith (<>) a1 a2) sk1 sr1
